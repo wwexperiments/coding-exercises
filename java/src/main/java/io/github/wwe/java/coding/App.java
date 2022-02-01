0c230744f9080efe22e890cdecb14d5f0b834f39
Bala Raman <srbala@gmail.com>
2022-02-01 00:34:30 -0500

    Basic maven setup

---
+++ java/src/main/java/io/github/wwe/java/coding/App.java
@@ -0,0 +1,13 @@
+package io.github.wwe.java.coding;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
